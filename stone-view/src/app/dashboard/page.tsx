fda0238ee6eba70cca9bdef3f0dc1c00b4cfff10
kaketsu <huajiewuxi@gmail.com>
2024-11-14 20:05:18 +0800

    update ui

--- stone-view/src/app/dashboard/page.tsx
+++ stone-view/src/app/dashboard/page.tsx
@@ -3,26 +3,25 @@
 import { Dashboard } from '@/types/index'
 import { getDashboard } from '@/utils/service'
 import { Column } from '@ant-design/plots'
-import { Card } from 'antd'
-import { useEffect, useState } from 'react'
+import { Card, Col, Row, Statistic, Table } from 'antd'
+import dayjs from 'dayjs'
+import { useEffect, useMemo, useState } from 'react'
 
 export default function DashboardPage() {
-  const [data, setData] = useState<Dashboard[]>([])
+  const [dashboardList, setDashboardList] = useState<Dashboard[]>([])
   const [currentTab, setCurrentTab] = useState('tab1')
   useEffect(() => {
     asyncFetch()
   }, [])
 
-  useEffect(() => {}, [])
-
   const asyncFetch = () => {
     getDashboard().then((data) => {
-      setData(data)
+      setDashboardList(data)
     })
   }
 
   const config = {
-    data,
+    data: dashboardList,
     xField: 'date',
     yField: 'tradingVolume',
     xAxis: {
@@ -56,9 +55,118 @@ export default function DashboardPage() {
     tab2: <p>content2</p>,
   }
 
+  const columns = useMemo(() => {
+    return [
+      {
+        title: 'date',
+        dataIndex: 'date',
+        key: 'date',
+        width: 120,
+        render: (val: string) => {
+          return dayjs(val).format('YYYY-MM-DD')
+        },
+      },
+      {
+        title: 'tradingVolume',
+        dataIndex: 'tradingVolume',
+        key: 'tradingVolume',
+      },
+      {
+        title: 'limitUpCount1',
+        dataIndex: 'limitUpCount1',
+        key: 'limitUpCount1',
+      },
+      {
+        title: 'limitUpCount2',
+        dataIndex: 'limitUpCount2',
+        key: 'limitUpCount2',
+      },
+      {
+        title: 'limitUpCount3',
+        dataIndex: 'limitUpCount3',
+        key: 'limitUpCount3',
+      },
+      {
+        title: 'limitUpCountBeforeCallAuction',
+        dataIndex: 'limitUpCountBeforeCallAuction',
+        key: 'limitUpCountBeforeCallAuction',
+      },
+      {
+        title: 'limitDownCount1',
+        dataIndex: 'limitDownCount1',
+        key: 'limitDownCount1',
+      },
+      {
+        title: 'limitDownCount2',
+        dataIndex: 'limitDownCount2',
+        key: 'limitDownCount2',
+      },
+      {
+        title: 'limitDownCount3',
+        dataIndex: 'limitDownCount3',
+        key: 'limitDownCount3',
+      },
+      {
+        title: 'limitDownCountBeforeCallAuction',
+        dataIndex: 'limitDownCountBeforeCallAuction',
+        key: 'limitDownCountBeforeCallAuction',
+      },
+      {
+        title: 'redStockCount',
+        dataIndex: 'redStockCount',
+        key: 'redStockCount',
+      },
+    ]
+  }, [])
+
+  useEffect(() => {}, [])
+
   return (
     <div>
-      <Card
+      <Card style={{ width: '100%' }} title="Dashboard" bordered={false}>
+        <Table
+          columns={columns}
+          dataSource={dashboardList}
+          scroll={{ x: 'max-content' }}
+          expandable={{
+            expandedRowRender: (record: any) => {
+              return (
+                <Row gutter={16}>
+                  <Col span={4}>
+                    <Statistic
+                      title="tradingVolume1"
+                      value={record.tradingVolume1}
+                      valueStyle={{
+                        fontSize: 16,
+                      }}
+                    />
+                  </Col>
+                  <Col span={4}>
+                    <Statistic
+                      title="tradingVolume2"
+                      value={record.tradingVolume2}
+                      valueStyle={{
+                        fontSize: 16,
+                      }}
+                    />
+                  </Col>
+                  <Col span={4}>
+                    <Statistic
+                      title="tradingVolume2"
+                      value={record.tradingVolume3}
+                      valueStyle={{
+                        fontSize: 16,
+                      }}
+                    />
+                  </Col>
+                </Row>
+              )
+            },
+            // rowExpandable: (record) => record.name !== 'Not Expandable',
+          }}
+        />
+      </Card>
+      {/* <Card
         style={{ width: '100%' }}
         title="Card title"
         tabList={tabList}
@@ -68,7 +176,7 @@ export default function DashboardPage() {
         }}
       >
         {contentList[currentTab]}
-      </Card>
+      </Card> */}
     </div>
   )
 }
--- stone-view/src/app/layout.tsx
+++ stone-view/src/app/layout.tsx
@@ -1,70 +1,70 @@
-"use client";
+'use client'
 
-import "./globals.css";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { DesktopOutlined } from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
-const { Header, Content, Footer, Sider } = Layout;
-type MenuItem = Required<MenuProps>["items"][number];
+import { DesktopOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
+import type { MenuProps } from 'antd'
+import { Breadcrumb, Button, Layout, Menu } from 'antd'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import './globals.css'
+import styles from './style.module.scss'
+const { Header, Content, Footer, Sider } = Layout
+type MenuItem = Required<MenuProps>['items'][number]
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [collapsed, setCollapsed] = useState(false);
-  const router = useRouter();
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [collapsed, setCollapsed] = useState(false)
+  const router = useRouter()
+
   const items: MenuItem[] = [
     {
-      key: "dashboard",
-      label: "Dashboard",
+      key: 'dashboard',
+      label: 'Dashboard',
       icon: <DesktopOutlined />,
       onClick: () => {
-        router.push("/dashboard");
+        router.push('/dashboard')
       },
     },
-  ];
+  ]
 
   return (
     <html lang="zh">
       <body className="flex">
-        <div className="flex-1 flex flex-col">
-          <Header style={{ display: "flex", alignItems: "center" }}>
-            <div className="demo-logo" />
-          </Header>
-          <Layout className="flex-1">
-            <Sider width={200} collapsed>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
-                style={{ height: "100%", borderRight: 0 }}
-                items={items}
-              />
-            </Sider>
-            <Layout className="px-6 pb-6">
-              <Breadcrumb className="my-4">
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
-              <Content
-                style={{
-                  padding: 16,
-                  margin: 0,
-                  minHeight: 280,
-                  background: "#fff",
-                  borderRadius: "4px",
-                }}
-              >
+        <AntdRegistry>
+          <div className="flex flex-1 flex-col">
+            <Header className="bg-">
+              <div className="demo-logo" />
+            </Header>
+            <Layout className="flex-1">
+              <Sider width={200} collapsed={collapsed}>
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  items={items}
+                  className={styles.stoneMenu}
+                />
+              </Sider>
+              <Layout className="px-6 pb-6">
+                <div className="my-4 flex items-center">
+                  <Button
+                    className="mr-2"
+                    type="text"
+                    icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                    onClick={() => setCollapsed(!collapsed)}
+                  />
+                  <Breadcrumb className="h-[26px]">
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                  </Breadcrumb>
+                </div>
+
                 {children}
-              </Content>
+              </Layout>
             </Layout>
-          </Layout>
-        </div>
+          </div>
+        </AntdRegistry>
       </body>
     </html>
-  );
+  )
 }
